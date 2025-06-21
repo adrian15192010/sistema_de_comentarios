3a2dc8ba62e894e65dc681282942274b56ab4451
Diego Gonzalez <adrian15192010@gmail.com>
2025-06-21 09:08:41 -0400

    asdasd

--- publicacion/src/main/java/com/microservice/publicacion/controller/ReaccionController.java
+++ publicacion/src/main/java/com/microservice/publicacion/controller/ReaccionController.java
@@ -19,6 +19,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
+@CrossOrigin
 @RestController
 @RequestMapping("/api/reaccion")
 public class ReaccionController {
