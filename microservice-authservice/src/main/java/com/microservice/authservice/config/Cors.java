57cb1ac2e8d1cae71aba29331ef289dc3d8f12ba
Diego Gonzalez <adrian15192010@gmail.com>
2025-06-25 18:30:55 -0400

    adasd

--- microservice-authservice/src/main/java/com/microservice/authservice/config/Cors.java
+++ microservice-authservice/src/main/java/com/microservice/authservice/config/Cors.java
@@ -12,7 +12,7 @@ public class Cors implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://127.0.0.1:5500")
+                .allowedOrigins("http://localhost:4200")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
                 .allowCredentials(true);
